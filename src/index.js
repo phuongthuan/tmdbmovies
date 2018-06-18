8d0c053de3c6c4c81c018875fa3759d2106c5571
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-06-18 17:49:11 +0700

    config test for react app

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import fontawesome from '@fortawesome/fontawesome'
-import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
-import faArrowRight from '@fortawesome/fontawesome-free-solid/faArrowAltCircleRight';
-import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowAltCircleLeft';
-
+import { faSearch, faArrowRight, faArrowLeft } from '@fortawesome/fontawesome-free-solid';
 
 import App from './app/containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 fontawesome.library.add(faSearch, faArrowRight, faArrowLeft);
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
