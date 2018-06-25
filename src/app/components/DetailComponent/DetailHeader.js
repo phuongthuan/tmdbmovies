3568e7f4e50dc232b6f5c65d92f49a23f55695b1
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-06-25 10:54:31 +0700

    dynamic background image á»url

--- src/app/components/DetailComponent/DetailHeader.js
+++ src/app/components/DetailComponent/DetailHeader.js
@@ -2,35 +2,63 @@ import React from 'react';
 import moment from 'moment';
 import image from '../../images/not-available.png';
 import Icon from '@fortawesome/react-fontawesome';
-
+import glamorous from 'glamorous';
 
 
 const DetailHeader = (props) => {
 
-    const { poster_path, title, release_date, overview } = props.data;
-    let imageUrl = props.data.poster_path == null ? image : `https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`;
+    const { backdrop_path, poster_path, title, release_date, overview} = props.data;
+
+    let imageUrl = poster_path == null ? image : `https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`;
+
+    function getBackgroundImage() {
+        if (backdrop_path) {
+            return `url('https://image.tmdb.org/t/p/w1400_and_h450_face${backdrop_path}')`;
+        }
+    }
+
+    function randomRGBA() {
+        const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+        const randomByte = () => randomNumber(0, 255);
+        const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2);
+        const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(',')})`;
+        return {
+            backgroundImage: `radial-gradient(circle at 20% 50%, ${randomCssRgba()} 0%, ${randomCssRgba()} 100%)`,
+        };
+    }
+
+    const DetailHeader = glamorous.div(
+        {
+            '&::before': {
+                backgroundImage: `${getBackgroundImage()}`
+            }
+        }
+    );
 
     return (
-        <div className="detail_header large border first">
-            <div className="custom_bg">
-                <div className="single_column">
-                    <section className="images">
-                        <div className="poster">
-                            <div className="image_content">
-                                <img src={imageUrl} alt={title} />
-                            </div>
-                            <div className="zoom">
-                                <a href=""><span><Icon icon="search-plus" /></span>Expand</a>
-                            </div>
-                        </div>
-                        <div className="header_poster_wrapper">
-                            <section className="header poster">
-                                <div className="title">
-                                          <span>
-                                            <a href=""><h2>{title}</h2></a><span className="release_date">({moment(release_date).format("YYYY")})</span>
-                                          </span>
+        <div>
+            <DetailHeader className="detail_header large border first">
+
+                <div className="custom_bg" style={randomRGBA()}>
+                    <div className="single_column">
+                        <section className="images">
+                            <div className="poster">
+                                <div className="image_content">
+                                    <img src={imageUrl} alt={title}/>
                                 </div>
-                                {/* <ul class="auto actions">
+                                <div className="zoom">
+                                    <a href=""><span><Icon icon="search-plus"/></span>Expand</a>
+                                </div>
+                            </div>
+                            <div className="header_poster_wrapper">
+                                <section className="header poster">
+                                    <div className="title">
+                                          <span>
+                                            <a href=""><h2>{title}</h2></a><span
+                                              className="release_date">({moment(release_date).format("YYYY")})</span>
+                                          </span>
+                                    </div>
+                                    {/* <ul class="auto actions">
                                                 <li class="chart">
                                                     <div class="consensus">
                                                         <div class="outer_ring">
@@ -52,28 +80,29 @@ const DetailHeader = (props) => {
                                                 <li>50</li>
                                                 <li>60</li>
                                             </ul> */}
-                                <div className="header_info">
-                                    <h3>Overview</h3>
-                                    <div className="overview">
-                                        <p>{overview}</p>
+                                    <div className="header_info">
+                                        <h3>Overview</h3>
+                                        <div className="overview">
+                                            <p>{overview}</p>
+                                        </div>
+                                        <h3 className="featured">Featured Crew</h3>
+                                        <ol className="people no_image">
+                                            <li className="profile">
+                                                <p><a href="">Frank Darabont</a></p>
+                                                <p className="character">Director, Writer</p>
+                                            </li>
+                                            <li className="profile">
+                                                <p><a href="">Stephen King</a></p>
+                                                <p className="character">Novel</p>
+                                            </li>
+                                        </ol>
                                     </div>
-                                    <h3 className="featured">Featured Crew</h3>
-                                    <ol className="people no_image">
-                                        <li className="profile">
-                                            <p><a href="">Frank Darabont</a></p>
-                                            <p className="character">Director, Writer</p>
-                                        </li>
-                                        <li className="profile">
-                                            <p><a href="">Stephen King</a></p>
-                                            <p className="character">Novel</p>
-                                        </li>
-                                    </ol>
-                                </div>
-                            </section>
-                        </div>
-                    </section>
+                                </section>
+                            </div>
+                        </section>
+                    </div>
                 </div>
-            </div>
+            </DetailHeader>
         </div>
     );
 };
